86048c1980c19b5c8b0f71161fdea9d2afdb693e
momov9 <r0905334@student.thomasmore.be>
2022-02-20 23:12:59 +0100

    test

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,16 +1,26 @@
-package bpackage be.thomasmore.party.controllers;
+package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        String myName = "Mohamed Ahnoud";
+        String myStreet = "Lageweg 451";
+        String myCity = "Hoboken";
+        model.addAttribute("name", myName);
+        model.addAttribute("street", myStreet);
+        model.addAttribute("city", myCity);
         return "about";
     }
 }
